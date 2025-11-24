9cc8f559564cd04cea543c6b27909d753875e66a
Daniel Samoha <danielaharon.samoha@post.runi.ac.il>
2025-11-25 01:32:04 +0200

    update

--- MyString.java
+++ MyString.java
@@ -24,8 +24,6 @@ public class MyString {
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        str1 = lowerCase(str1);
-        str2 = lowerCase(str2);
         if (str2.length() == 0){
             return true;
         }
--- Primes.java
+++ Primes.java
@@ -2,6 +2,7 @@ public class Primes {
     public static void main(String[] args) {
         int mone = 0;
         int p = Integer.parseInt(args[0]);
+        System.out.println("Prime numbers up to " + p + ":");
         for (int i = 2; i <= p; i++){
             Boolean isprime =true;
             for (int j = 2; j < i && isprime == true; j++){
@@ -14,7 +15,7 @@ public class Primes {
                 mone++;
             }
         }
-        System.out.println("Prime numbers up to " + p + ":");
+        
         int pre = (int)((double)mone / (double)(p) * 100);
         System.out.println("There are " + mone + " primes between 2 and " + p + " (" + pre + "% are primes)");
     }
